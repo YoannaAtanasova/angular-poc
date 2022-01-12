5e218595b9b3d9bfc93aa078bb0a0a90e1db0b85
Yoanna Atanasova <atanasova.yoanna@gmail.com>
2022-01-12 15:09:33 +0200

    Change title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { filter, takeUntil } from 'rxjs/operators';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit, OnDestroy {
-  title = 'msal-angular-tutorial';
+  title = 'angular-poc';
   isIframe = false;
   loginDisplay = false;
   private readonly _destroying$ = new Subject<void>();
